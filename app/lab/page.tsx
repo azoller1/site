dafb12ff943476d4ca099a5188aaa104d9e96a33
Alexander Zoller <personal@alexanderzoller.com>
2024-11-23 16:59:46 -0600

    add pic and change some padding

--- app/lab/page.tsx
+++ app/lab/page.tsx
@@ -7,7 +7,7 @@ export default function Page() {
       <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Lab</h1>
         <p>A rough overview/sketch of my homelab and its servers with cloud servers that are relevant. Setting
           up everything is enjoyable and has allowed me to learn many new technologies and how to setup network infrastructure
-          (Always room for improvement, certainly not setup for prod ;).
+          (Always room for improvement. Certainly not setup for prod ;).
         </p>
       <br />
       <Image
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image'
+import profilePic from './profile_pic.jpg'
+
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">About Me</h1>
+      <Image
+      src={profilePic}
+      width={300}
+      height={200}
+      alt="Profile Pic"
+      />
+      <br />
       <p className="mb-4">
         {`Hello! My name is Alexander. I enjoy working with computers and creating things.
          Click on 'lab' above to see information about my private homelab I have for enjoyment
